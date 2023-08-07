8a3ebda32d400d19291320c2f09bf3961d79ec48
Harrison Chase <hw.chase.17@gmail.com>
2023-08-07 15:50:59 -0700

    cr

---
+++ code_snippets.py
@@ -0,0 +1,57 @@
+CHARACTER = """```python
+from langchain.text_splitter import CharacterTextSplitter
+
+{length_function}
+
+splitter = CharacterTextSplitter(
+    separator = "\\n\\n",  # Split character (default \\n\\n)
+    chunk_size={chunk_size},
+    chunk_overlap={chunk_overlap},
+    length_function=length_function,
+)
+text = "foo bar"
+splits = splitter.split_text(text)
+"""
+
+RECURSIVE_CHARACTER = """```python
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+{length_function}
+
+# The default list of split characters is [\\n\\n, \\n, " ", ""]
+# Tries to split on them in order until the chunks are small enough
+# Keep paragraphs, sentences, words together as long as possible
+splitter = RecursiveCharacterTextSplitter(
+    separators=[\\n\\n, \\n, " ", ""],
+    chunk_size={chunk_size}, 
+    chunk_overlap={chunk_overlap},
+    length_function=length_function,
+)
+text = "foo bar"
+splits = splitter.split_text(text)
+"""
+
+LANGUAGE = """```python
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+
+{length_function}
+
+splitter = RecursiveCharacterTextSplitter.from_language(
+    {language},
+    chunk_size={chunk_size}, 
+    chunk_overlap={chunk_overlap},
+    length_function=length_function,
+)
+text = "foo bar"
+splits = splitter.split_text(text)
+"""
+
+CHARACTER_LENGTH = "length_function = len"
+
+TOKEN_LENGTH = """import tiktoken
+
+enc = tiktoken.get_encoding("cl100k_base")
+    
+def length_function(text: str) -> int:
+    return len(enc.encode(text))
+"""
--- splitter.py
+++ splitter.py
@@ -1,21 +1,29 @@
 import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter, Language
+import code_snippets as code_snippets
+import tiktoken
 
 
 # Streamlit UI
-st.title("Document Splitter Playground")
-st.info("Split a document into chunks using a `Document Splitter` with input `chunk_size` and `chunk_overlap`")
-col1, col2, col3 = st.columns([1, 1, 1])
+st.title("Text Splitter Playground")
+st.info("""Split a text into chunks using a **Text Splitter**. Parameters include:
+
+- `chunk_size`: Max size of the resulting chunks (in either characters or tokens, as selected)
+- `chunk_overlap`: Overlap between the resulting chunks (in either characters or tokens, as selected)
+- `length_function`: How to measure lengths of chunks, examples are included for either characters or tokens
+- The type of the text splitter, this largely controls the separators used to split on
+""")
+col1, col2, col3, col4 = st.columns([1, 1, 1, 2])
 
 with col1:
-    chunk_size = st.number_input(min_value=1, label="Chunk Size (Characters or Tokens)", value=1000)
+    chunk_size = st.number_input(min_value=1, label="Chunk Size", value=1000)
 
 with col2:
     # Setting the max value of chunk_overlap based on chunk_size
     chunk_overlap = st.number_input(
         min_value=1,
         max_value=chunk_size - 1,
-        label="Chunk Overlap (Characters or Tokens)",
+        label="Chunk Overlap",
         value=int(chunk_size * 0.2),
     )
 
@@ -24,70 +32,82 @@ with col2:
         st.warning("Chunk Overlap should be less than Chunk Length!")
 
 with col3:
+    length_function = st.selectbox(
+        "Length Function", ["Characters", "Tokens"]
+    )
+
+splitter_choices = ["RecursiveCharacter", "Character"] + [str(v) for v in Language]
+
+with col4:
     splitter_choice = st.selectbox(
-        "Select a Document Splitter", ["Characters", "Recursive Characters", "Tokens"]
+        "Select a Text Splitter", splitter_choices
     )
 
-if splitter_choice == "Characters":
-    import_text = """```python
-    from langchain.text_splitter import CharacterTextSplitter
+if length_function == "Characters":
+    length_function = len
+    length_function_str = code_snippets.CHARACTER_LENGTH
+elif length_function == "Tokens":
+    enc = tiktoken.get_encoding("cl100k_base")
 
-    splitter = CharacterTextSplitter(
-        separator = " ", # Split character (default \\n\\n)
-        chunk_size={chunk_size}, # Measure chunk length by number of characters
-        chunk_overlap={chunk_overlap}
+
+    def length_function(text: str) -> int:
+        return len(enc.encode(text))
+
+
+    length_function_str = code_snippets.TOKEN_LENGTH
+else:
+    raise ValueError
+
+if splitter_choice == "Character":
+    import_text = code_snippets.CHARACTER.format(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=length_function_str
     )
-    text = "foo bar"
-    splits = splitter.split_text(text)
-    """.format(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 
-elif splitter_choice == "Recursive Characters":
-    import_text = """```python
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    # The default list of split characters is [\\n\\n, \\n, " ", ""]
-    # Tries to split on them in order until the chunks are small enough
-    # Keep paragraphs, sentences, words together as long as possible
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size={chunk_size}, 
-        chunk_overlap={chunk_overlap}
+elif splitter_choice == "RecursiveCharacter":
+    import_text = code_snippets.RECURSIVE_CHARACTER.format(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=length_function_str
     )
-    text = "foo bar"
-    splits = splitter.split_text(text)
-    """.format(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 
-else: # Tokens
-    import_text = """```python
-    from langchain.text_splitter import CharacterTextSplitter
-
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator = " ", # Split character (default \\n\\n)
-        chunk_size={chunk_size}, # Measure chunk length by number of characters
-        chunk_overlap={chunk_overlap}
+elif "Language." in splitter_choice:
+    import_text = code_snippets.LANGUAGE.format(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        language=splitter_choice,
+        length_function=length_function_str
     )
-    text = "foo bar"
-    splits = splitter.split_text(text)
-    """.format(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+else:
+    raise ValueError
 
 st.info(import_text)
 
-# Box for pasting document
-doc = st.text_area("Paste your document here:")
+# Box for pasting text
+doc = st.text_area("Paste your text here:")
 
-# Split document button
-if st.button("Split Document"):
+# Split text button
+if st.button("Split Text"):
     # Choose splitter
-    if splitter_choice == "Characters":
-        splitter = CharacterTextSplitter(separator = " ", 
+    if splitter_choice == "Character":
+        splitter = CharacterTextSplitter(separator = "\n\n",
                                          chunk_size=chunk_size, 
-                                         chunk_overlap=chunk_overlap)
-    elif splitter_choice == "Recursive Characters":
+                                         chunk_overlap=chunk_overlap,
+                                         length_function=length_function)
+    elif splitter_choice == "RecursiveCharacter":
         splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, 
-                                                  chunk_overlap=chunk_overlap)
-    else: # Tokens
-        splitter = CharacterTextSplitter.from_tiktoken_encoder(separator = " ",
-                                                               chunk_size=chunk_size,
-                                                               chunk_overlap=chunk_overlap)
-    # Split the document
+                                                  chunk_overlap=chunk_overlap,
+                                         length_function=length_function)
+    elif "Language." in splitter_choice:
+        language = splitter_choice.split(".")[1].lower()
+        splitter = RecursiveCharacterTextSplitter.from_language(language,
+                                                                chunk_size=chunk_size,
+                                                                chunk_overlap=chunk_overlap,
+                                         length_function=length_function)
+    else:
+        raise ValueError
+    # Split the text
     splits = splitter.split_text(doc)
 
     # Display the splits
